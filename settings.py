dcee5562600bcecea053b0f3cf3fa5f8c776a05b
Turki <t.alelyani@hotmail.com>
2025-05-08 18:39:31 +0300

    Delete settings.py

--- settings.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-class Config:
-    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-    EMBEDDING_MODEL = "distilbert-base-uncased"
-    SUMMARIZATION_MODEL = "sshleifer/distilbart-cnn-12-6"
-    MAX_INPUT_LENGTH = 10000
-    CHUNK_SIZE = 1000
-    CLEANUP_EVERY = 3
